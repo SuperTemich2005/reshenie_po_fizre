fe45b88fbaa6c7f8e09089f7743382bb9789b5ef
SuperTemich2005 <39464061+SuperTemich2005@users.noreply.github.com>
2021-11-10 13:01:44 +0200

    Add files via upload

---
+++ fuckyou!.py
@@ -0,0 +1,9 @@
+while True: #do
+    volanchik = int(input("volanchik "))
+    sapogi = int(input("sapogi "))
+    raketka = int(input("raketka "))
+    print(volanchik*3)
+    print(raketka+volanchik)
+    print(sapogi+volanchik)
+    #bullshit
+    print(raketka-sapogi)
